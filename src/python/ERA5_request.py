5c5a9330b0ec48bf608d690fe3f764bbd99cad07
CatraMyBeloved <ole.stein@alumni.fh-aachen.de>
2025-03-19 19:01:21 +0100

    starting eda to understand datasets

--- src/python/ERA5_request.py
+++ src/python/ERA5_request.py
@@ -1,18 +1,16 @@
 import cdsapi
 
-dataset = "reanalysis-era5-single-levels-monthly-means"
+dataset = "reanalysis-era5-land-monthly-means"
 request = {
     "product_type": ["monthly_averaged_reanalysis"],
     "variable": [
         "2m_temperature",
-        "sea_surface_temperature",
-        "total_precipitation",
-        "volumetric_soil_water_layer_1"
+        "lake_total_layer_temperature",
+        "snow_cover",
+        "volumetric_soil_water_layer_1",
+        "total_precipitation"
     ],
-    "year": [
-        "2016", "2017", "2018",
-        "2019", "2020"
-    ],
+    "year": ["2019"],
     "month": [
         "01", "02", "03",
         "04", "05", "06",
@@ -21,10 +19,8 @@ request = {
     ],
     "time": ["00:00"],
     "data_format": "netcdf",
-    "download_format": "unarchived"
+    "download_format": "zip"
 }
 
 client = cdsapi.Client()
-client.retrieve(dataset, request, target= "data/example.zip")
-
-
+client.retrieve(dataset, request, "data/example.zip")
